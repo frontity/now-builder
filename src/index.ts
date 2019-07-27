5c47a1d50ec0f36d72d3ad35d98b3afb51a34bd9
Luis Herranz <luisherranz@gmail.com>
2019-07-28 00:13:40 +0200

    Remove frameworks and now-dev

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import path from 'path';
-import spawn from 'cross-spawn';
-import getPort from 'get-port';
-import { timeout } from 'promise-timeout';
-import { existsSync, readFileSync, statSync, readdirSync } from 'fs';
-import frameworks from './frameworks';
+import path from "path";
+import { existsSync, readFileSync, statSync, readdirSync } from "fs";
 import {
   glob,
   download,
@@ -15,8 +11,8 @@ import {
   Files,
   Route,
   BuildOptions,
-  Config,
-} from '@now/build-utils';
+  Config
+} from "@now/build-utils";
 
 interface PackageJson {
   scripts?: {
@@ -30,14 +26,6 @@ interface PackageJson {
   };
 }
 
-interface Framework {
-  name: string;
-  dependency: string;
-  getOutputDirName: (dirPrefix: string) => Promise<string>;
-  defaultRoutes?: Route[];
-  minNodeRange?: string;
-}
-
 function validateDistDir(
   distDir: string,
   isDev: boolean | undefined,
@@ -49,12 +37,12 @@ function validateDistDir(
   const isEmpty = () => readdirSync(distDir).length === 0;
 
   const hash = isDev
-    ? '#local-development'
-    : '#configuring-the-build-output-directory';
+    ? "#local-development"
+    : "#configuring-the-build-output-directory";
   const docsUrl = `https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build${hash}`;
 
   const info = config.zeroConfig
-    ? '\nMore details: https://zeit.co/docs/v2/advanced/platform/frequently-asked-questions#missing-public-directory'
+    ? "\nMore details: https://zeit.co/docs/v2/advanced/platform/frequently-asked-questions#missing-public-directory"
     : `\nMake sure you configure the the correct distDir: ${docsUrl}`;
 
   if (!exists()) {
@@ -78,7 +66,7 @@ function getCommand(pkg: PackageJson, cmd: string, { zeroConfig }: Config) {
   // The `dev` script can be `now dev`
   const nowCmd = `now-${cmd}`;
 
-  if (!zeroConfig && cmd === 'dev') {
+  if (!zeroConfig && cmd === "dev") {
     return nowCmd;
   }
 
@@ -97,29 +85,14 @@ function getCommand(pkg: PackageJson, cmd: string, { zeroConfig }: Config) {
 
 export const version = 2;
 
-const nowDevScriptPorts = new Map();
-
-const getDevRoute = (srcBase: string, devPort: number, route: Route) => {
-  const basic: Route = {
-    src: `${srcBase}${route.src}`,
-    dest: `http://localhost:${devPort}${route.dest}`,
-  };
-
-  if (route.headers) {
-    basic.headers = route.headers;
-  }
-
-  return basic;
-};
-
 export async function build({
   files,
   entrypoint,
   workPath,
   config,
-  meta = {},
+  meta = {}
 }: BuildOptions) {
-  console.log('Downloading user files...');
+  console.log("Downloading user files...");
   await download(files, workPath, meta);
 
   const mountpoint = path.dirname(entrypoint);
@@ -128,198 +101,55 @@ export async function build({
   let distPath = path.join(
     workPath,
     path.dirname(entrypoint),
-    (config && (config.distDir as string)) || 'build'
+    (config && (config.distDir as string)) || "build"
   );
 
   const entrypointName = path.basename(entrypoint);
 
-  if (entrypointName === 'package.json') {
+  if (entrypointName === "package.json") {
     const pkgPath = path.join(workPath, entrypoint);
-    const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
+    const pkg = JSON.parse(readFileSync(pkgPath, "utf8"));
 
     let output: Files = {};
-    let framework: Framework | undefined = undefined;
     let minNodeRange: string | undefined = undefined;
 
     const routes: Route[] = [];
-    const devScript = getCommand(pkg, 'dev', config as Config);
-
-    if (config.zeroConfig) {
-      // `public` is the default for zero config
-      distPath = path.join(workPath, path.dirname(entrypoint), 'public');
-
-      const dependencies = Object.assign(
-        {},
-        pkg.dependencies,
-        pkg.devDependencies
-      );
-
-      framework = frameworks.find(({ dependency }) => dependencies[dependency]);
-    }
-
-    if (framework) {
-      console.log(
-        `Detected ${framework.name} framework. Optimizing your deployment...`
-      );
-
-      if (framework.minNodeRange) {
-        minNodeRange = framework.minNodeRange;
-        console.log(
-          `${framework.name} requires Node.js ${
-            framework.minNodeRange
-          }. Switching...`
-        );
-      } else {
-        console.log(
-          `${
-            framework.name
-          } does not require a specific Node.js version. Continuing ...`
-        );
-      }
-    }
 
     const nodeVersion = await getNodeVersion(entrypointDir, minNodeRange);
     const spawnOpts = getSpawnOptions(meta, nodeVersion);
 
-    await runNpmInstall(entrypointDir, ['--prefer-offline'], spawnOpts);
+    await runNpmInstall(entrypointDir, ["--prefer-offline"], spawnOpts);
 
-    if (meta.isDev && pkg.scripts && pkg.scripts[devScript]) {
-      let devPort: number | undefined = nowDevScriptPorts.get(entrypoint);
+    const buildScript = getCommand(pkg, "build", config as Config);
+    console.log(`Running "${buildScript}" script in "${entrypoint}"`);
 
-      if (framework && framework.defaultRoutes) {
-        // We need to delete the routes for `now dev`
-        // since in this case it will get proxied to
-        // a custom server we don't have controll over
-        delete framework.defaultRoutes;
-      }
+    const found = await runPackageJsonScript(
+      entrypointDir,
+      buildScript,
+      spawnOpts
+    );
 
-      if (typeof devPort === 'number') {
-        console.log(
-          '`%s` server already running for %j',
-          devScript,
-          entrypoint
-        );
-      } else {
-        // Run the `now-dev` or `dev` script out-of-bounds, since it is assumed that
-        // it will launch a dev server that never "completes"
-        devPort = await getPort();
-        nowDevScriptPorts.set(entrypoint, devPort);
-
-        const opts = {
-          cwd: entrypointDir,
-          env: { ...process.env, PORT: String(devPort) },
-        };
-
-        const child = spawn('yarn', ['run', devScript], opts);
-        child.on('exit', () => nowDevScriptPorts.delete(entrypoint));
-        if (child.stdout) {
-          child.stdout.setEncoding('utf8');
-          child.stdout.pipe(process.stdout);
-        }
-        if (child.stderr) {
-          child.stderr.setEncoding('utf8');
-          child.stderr.pipe(process.stderr);
-        }
-
-        // Now wait for the server to have listened on `$PORT`, after which we
-        // will ProxyPass any requests to that development server that come in
-        // for this builder.
-        try {
-          await timeout(
-            new Promise(resolve => {
-              const checkForPort = (data: string) => {
-                // Check the logs for the URL being printed with the port number
-                // (i.e. `http://localhost:47521`).
-                if (data.indexOf(`:${devPort}`) !== -1) {
-                  resolve();
-                }
-              };
-              if (child.stdout) {
-                child.stdout.on('data', checkForPort);
-              }
-              if (child.stderr) {
-                child.stderr.on('data', checkForPort);
-              }
-            }),
-            5 * 60 * 1000
-          );
-        } catch (err) {
-          throw new Error(
-            `Failed to detect a server running on port ${devPort}.\nDetails: https://err.sh/zeit/now-builders/now-static-build-failed-to-detect-a-server`
-          );
-        }
-
-        console.log('Detected dev server for %j', entrypoint);
-      }
-
-      let srcBase = mountpoint.replace(/^\.\/?/, '');
-
-      if (srcBase.length > 0) {
-        srcBase = `/${srcBase}`;
-      }
-
-      if (framework && framework.defaultRoutes) {
-        for (const route of framework.defaultRoutes) {
-          routes.push(getDevRoute(srcBase, devPort, route));
-        }
-      }
-
-      routes.push(
-        getDevRoute(srcBase, devPort, {
-          src: '/(.*)',
-          dest: '/$1',
-        })
+    if (!found) {
+      throw new Error(
+        `Missing required "${buildScript}" script in "${entrypoint}"`
       );
-    } else {
-      if (meta.isDev) {
-        console.log(`WARN: "${devScript}" script is missing from package.json`);
-        console.log(
-          'See the local development docs: https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build/#local-development'
-        );
-      }
-
-      const buildScript = getCommand(pkg, 'build', config as Config);
-      console.log(`Running "${buildScript}" script in "${entrypoint}"`);
-
-      const found = await runPackageJsonScript(
-        entrypointDir,
-        buildScript,
-        spawnOpts
-      );
-
-      if (!found) {
-        throw new Error(
-          `Missing required "${buildScript}" script in "${entrypoint}"`
-        );
-      }
-
-      if (framework) {
-        const outputDirPrefix = path.join(workPath, path.dirname(entrypoint));
-        const outputDirName = await framework.getOutputDirName(outputDirPrefix);
-
-        distPath = path.join(outputDirPrefix, outputDirName);
-      }
-
-      validateDistDir(distPath, meta.isDev, config);
-      output = await glob('**', distPath, mountpoint);
-
-      if (framework && framework.defaultRoutes) {
-        routes.push(...framework.defaultRoutes);
-      }
     }
 
-    const watch = [path.join(mountpoint.replace(/^\.\/?/, ''), '**/*')];
+    validateDistDir(distPath, meta.isDev, config);
+    output = await glob("**", distPath, mountpoint);
+
+    const watch = [path.join(mountpoint.replace(/^\.\/?/, ""), "**/*")];
     return { routes, watch, output };
   }
 
-  if (!config.zeroConfig && entrypointName.endsWith('.sh')) {
+  if (!config.zeroConfig && entrypointName.endsWith(".sh")) {
     console.log(`Running build script "${entrypoint}"`);
     const nodeVersion = await getNodeVersion(entrypointDir);
     const spawnOpts = getSpawnOptions(meta, nodeVersion);
     await runShellScript(path.join(workPath, entrypoint), [], spawnOpts);
     validateDistDir(distPath, meta.isDev, config);
 
-    return glob('**', distPath, mountpoint);
+    return glob("**", distPath, mountpoint);
   }
 
   let message = `Build "src" is "${entrypoint}" but expected "package.json"`;
